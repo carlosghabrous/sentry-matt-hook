cbdb43fd947611c54a83752613d2ce469aabe5e1
carlosghabrous <carlos.ghabrous@plantix.net>
2022-07-11 17:43:45 +0200

    TEST: add test

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -3,11 +3,18 @@ import requests
 
 LOCAL_ADDR = "http://127.0.0.1:5000"
 API_ROUTE = "/sentry/to-mattermost"
+NGROK_HOST = "https://cb46-178-237-233-8.eu.ngrok.io"
 
-local_request_json = {"action":"created", "data":"some data", "actor":{"type": "user",
-  "id": 2038298,
-  "name": "Carlos",
-}}
+local_request_json = {
+    "action":"created", 
+    "data":"some data", 
+    "actor":
+        {
+            "type": "user",
+            "id": 2038298,
+            "name": "Carlos",
+        }
+    }
 
 
 def test_hello():
@@ -23,4 +30,10 @@ def test_local_post_request_to_hook():
 def test_local_post_request_to_non_implemented_hook_returns_404():
     x = requests.post(LOCAL_ADDR + API_ROUTE, headers={"sentry-hook-resource":"issue"}, json=local_request_json)
     assert x.status_code == 404
+
+
+def test_ngrok_request_to_hook():
+    x = requests.post(NGROK_HOST + API_ROUTE, headers={"Content-type":"application/json", "sentry-hook-resource":"event_alert"}, json=local_request_json)   
+    assert x.status_code == 200
+
 
